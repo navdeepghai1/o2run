b8435c6657c904b5fb5ef4c72251dded2ae2681c
Ubuntu <ubuntu@ip-172-31-0-103.us-west-2.compute.internal>
2017-09-07 11:04:38 +0000

    Website Developments

--- o2run/public/js/o2run.js
+++ o2run/public/js/o2run.js
@@ -2,4 +2,92 @@
 	Developer Navdeep Ghai
 */
 
+o2run = {};
 
+
+o2run.items = function(){
+
+	var me = this;
+	if (!($(".list-group-item"))){
+		return false;
+	}
+	$(".list-group-item").on("click", function(event){
+		event.preventDefault();
+		group = $(this).attr("group-name");
+		me.get_list(group);
+		
+	});		
+}
+
+o2run.items.prototype.get_list = function(group){
+
+	
+	this.total = 0;
+	var me = this;
+	var opts = {};
+	var args  = {};
+	me.clear();
+	args.cmd = "o2run.utils.get_items";
+	args.group = group;
+	opts.callback = function(data){
+		me.callback_get_list(data);
+	};
+	opts.args = args;
+	frappe.call(opts);	
+
+}
+
+o2run.items.prototype.callback_get_list = function(data){
+	this.data = data.message? data.message:[];
+	if(!this.data.length){
+		this.not_found();
+		return false;
+	}
+	this.update_list();
+}
+
+o2run.items.prototype.update_list = function(){
+
+	var me = this;
+	var total = me.total;
+	var data_len = me.data.length;
+	var temp_len = data_len - total;
+	var temp =  temp_len <= 0?0:temp_len;
+	var flag = temp <= 20?temp: temp-20;
+	for(var i=total;i<flag; i++){
+		
+		item = me.data[i];
+		me.render(item);
+		this.total++;
+	}
+}
+
+o2run.items.prototype.get_more = function(){
+	this.update_list();
+}
+
+
+o2run.items.prototype.render = function(item){
+	
+	$(".o2run-items").append(item);
+	
+}
+
+o2run.items.prototype.clear = function(){
+	$(".o2run-items").empty();
+}
+
+
+o2run.items.prototype.not_found = function(){
+	$(".o2run-items").append(
+			"<div class='col-lg-4'><h1> No Item Found</h1></div>"
+			);
+};
+
+frappe.ready(function(){
+
+	var loc = window.location.pathname.replace("/", "");
+	if(loc == "items"){
+		new  o2run.items();
+	}
+});
--- o2run/templates/pages/items.py
+++ o2run/templates/pages/items.py
@@ -1,9 +1,13 @@
 '''
+
 	Developer Navdeep Ghai
 '''
 
 import frappe
 from frappe import _, msgprint, throw
+
+no_cache = 1
+no_sitemap = 1
 
 def get_context(context):
 	item_groups = get_item_groups()
@@ -21,7 +25,7 @@ def get_item_groups():
 
 def get_items():
 
-	items = frappe.db.sql("""SELECT item.item_name, item.item_code, item.description, item.image, pl.price_list_rate, \
+	items = frappe.db.sql("""SELECT item.name, item.item_name, item.item_code, item.description, item.image, pl.price_list_rate, \
 				pl.currency  FROM `tabItem` item LEFT JOIN `tabItem Price` pl  ON item.item_code = \
 				pl.item_code WHERE selling=1 LIMIT 20""", as_dict=True)
 	return items
